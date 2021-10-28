469bcc22d2393d3484f8e6962794e617a45ec641
DaagXL <fattonyd@hotmail.com>
2021-10-28 14:48:43 -0400

    Added metamagic and mythic tweaks

---
+++ LegendsGrimoire/Tweaks/MetamagicTweak.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using Kingmaker.UnitLogic.Abilities;
+using Kingmaker.UnitLogic.Commands.Base;
+
+namespace LegendsGrimoire.Tweaks
+{
+    class MetamagicTweak
+    {
+        [HarmonyPatch(typeof(AbilityData), "RequireFullRoundAction", MethodType.Getter)]
+        [HarmonyPriority(Priority.Last)]
+        static class AbilityData_RequireFullRoundAction_Patch
+        {
+            static void Postfix(AbilityData __instance, ref bool __result)
+            {
+                if (__instance.RuntimeActionType == UnitCommand.CommandType.Standard)
+                {
+                    MetamagicData metamagicData = __instance.MetamagicData;
+                    if (metamagicData == null || !metamagicData.Has(Metamagic.Quicken))
+                    {
+                        __result = __instance.Blueprint.IsFullRoundAction;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LegendsGrimoire/Tweaks/MythicTweak.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.Designers.Mechanics.Facts;
+using LegendsGrimoire.Utilities;
+
+namespace LegendsGrimoire.Tweaks
+{
+    class MythicTweak
+    {
+        [HarmonyPatch(typeof(BlueprintsCache), "Init")]
+        static class BlueprintsCache_Init_Patch
+        {
+            static bool Initialized;
+
+            static void Postfix()
+            {
+                if (Initialized) return;
+                Initialized = true;
+                Logger.LogHeader("Mythic Tweaks");
+                TweakAbundantCasting();
+                TweakEnduringSpells();
+            }
+
+            static readonly BlueprintFeature enduringSpells = Resources.GetBlueprint<BlueprintFeature>("2f206e6d292bdfb4d981e99dcf08153f");
+            static readonly BlueprintFeature abundantCasting = Resources.GetBlueprint<BlueprintFeature>("cf594fa8871332a4ba861c6002480ec2");
+
+            public static void TweakEnduringSpells()
+            {
+                var enduringSpellsComponent = enduringSpells.GetComponent<EnduringSpells>();
+                enduringSpellsComponent.m_Greater = enduringSpells.ToReference<BlueprintUnitFactReference>();
+                Logger.LogPatch("Patched", enduringSpells);
+            }
+
+            public static void TweakAbundantCasting()
+            {
+                var abundantCastingAddSpellsPerDay = abundantCasting.GetComponent<AddSpellsPerDay>();
+                abundantCastingAddSpellsPerDay.Levels = new int[] {
+                    1, 2, 3, 4, 5, 6, 7, 8, 9
+                };
+                Logger.LogPatch("Patched", abundantCasting);
+            }
+        }
+    }
+}
